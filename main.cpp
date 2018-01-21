71ed81e1b157bd8ef3a4a5371ddcd1dfd56808d5
Joseph Anderson <jtanderson31@gmail.com>
2018-01-21 11:50:18 -0500

    Use getopt to allow program arguments

--- main.cpp
+++ main.cpp
@@ -22,14 +22,83 @@ void interrupt(int i){
   exit(i);
 };
 
+void init(int argc, char** argv){
+  // For utf-8 glyphs
+  setlocale(LC_ALL, "");
+
+  // register the handler to clean up ncurses on close
+  signal(SIGINT, interrupt);
+
+  int c;
+  bool has_log_option = false;
+
+  while (1) {
+    static struct option long_options[] = {
+      {"help",     no_argument,       0, '?'},
+      {"log-level", required_argument, 0, 'l'},
+      {0, 0, 0, 0}
+    };
+
+    /* getopt_long stores the option index here. */
+    int option_index = 0;
+
+    c = getopt_long (argc, argv, "?:l:", //"abc:d:f:",
+        long_options, &option_index);
+
+    /* Detect the end of the options. */
+    if (c == -1){
+      break;
+    }
+
+    switch (c) {
+      case '?':
+        printf("TextAdventure!\n");
+        printf("Executable usage:\n");
+        printf("\t./main [options]\n\n");
+        printf("Where [options] is one of the following:\n");
+        printf("\t--log-level=[level], -l [level]\n");
+        printf("\t\t[level] can be:\n");
+        printf("\t\t\tdebug\n");
+        printf("\t\t\tinfo\n");
+        printf("\t\t\terror (default)\n");
+        printf("\t--help, -?\n");
+        printf("\t\tPrints this message.\n");
+        exit(0);
+        break;
+      case 'l':
+        if (strcmp(optarg,"debug") == 0){
+          Logger::init(Logger::DEBUG);
+        } else if (strcmp(optarg,"info") == 0){
+          Logger::init(Logger::INFO);
+        } else {
+          printf("Invalid log level: %s\n", optarg);
+          exit(1);
+        }
+        has_log_option = true;
+        break;
+      default:
+        abort ();
+    }
+  }
+
+  if (!has_log_option){
+    Logger::init(Logger::ERROR);
+  }
+
+  Logger::debug("Loading...\n");
+
+  // initialize UI
+  Display::init();
+
+  // Seed the rng
+  srand(time(0));
+
+}
+
 int main(int argc, char** argv){
   // TODO: use gnu getopt to parse the args
 
-  setlocale(LC_ALL, "");
-
-  // Needs to come first in case any ctors use logging
-  Logger::init(Logger::DEBUG);
-  Logger::debug("Loading...\n");
+  init(argc, argv);
 
   char input;
   std::stack<GameState*> stateStack;
@@ -38,16 +107,6 @@ int main(int argc, char** argv){
 
   Player pc(20,5,10,5,"Human","Jimbo");
   World world;
-
-  signal(SIGINT, interrupt);
-
-  Display::init();
-  srand(time(0));
-
-  //mvwaddstr(Display::text_win, 10, 10, "\u2605");
-  //wrefresh(Display::text_win);
-  //getch();
-
   GameState* currentState = nullptr;
   TravelState* t = new TravelState("North");
   stateStack.push(t);
